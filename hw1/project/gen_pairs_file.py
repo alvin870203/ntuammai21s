35430f9f94c858552659e01d3fbed19f3c3319d9
alvin870203 <alvin870203@gmail.com>
2021-04-07 22:48:38 +0800

    testing gen_pairs_file.py function

--- hw1/project/gen_pairs_file.py
+++ hw1/project/gen_pairs_file.py
@@ -8,9 +8,22 @@ def gen_pairs_file(data_root, labels_file, pairs_file):
     """
     Generate the pairs file with same format as CPLFW's pairs file
     """
-    pairs_file_buf = open(train_file, 'w')
+    pairs_file_buf = open(pairs_file, 'w')
     image_list = os.listdir(data_root)
     #image_list.sort()
-    labels_list = []
-    labels_file_buf = open(pairs_file)
+    #labels_list = []
+    with open(pairs_file) as f:
+        labels_list = [line.rstrip('\n') for line in f]
+    for index, image_path in enumerate(image_list):
+        labels_idx = image_path.split('_')[2]
+        line = image_path + ' ' + labels_list[labels_idx]
+        pairs_file_buf.write(line + '\n')
+
+
+if __name__ == '__main__':
+    data_root = './data/test/closed_set/test_piars'
+    labels_file = './data/test/closed_set/labels.txt'
+    # file to be generate.
+    pairs_file = './data/files/closed_set/pairs_closed.txt'
+    gen_pairs_file(data_root, labels_file, pairs_file)
     
